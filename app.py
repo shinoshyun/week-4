a85a69a059bfaf087837d63ce756667574cdd851
shinoshyun <8367shyun@gmail.com>
2022-10-12 00:49:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+from cgitb import text
+from importlib.resources import path
+from tkinter.ttk import Style
 from flask import Flask
 from flask import request
 from flask import render_template
-app = Flask(__name__, static_folder="public",
+app = Flask(__name__, static_folder="static",
             static_url_path="/")  # __name__ 代表目前執行的模組
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/member")
+def member():
+    account = request.args.get("account")
+
+    # if account == "test"
+
+    return render_template("member.html")
 
 
 app.run(port=3000)
