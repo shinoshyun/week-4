af04a9408596c52dd73c66e6575e92d3ac03485e
shinoshyun <8367shyun@gmail.com>
2022-10-12 18:21:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from cgitb import text
-from importlib.resources import path
-from tkinter.ttk import Style
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template, session, redirect
+
+
 app = Flask(__name__, static_folder="static",
             static_url_path="/")  # __name__ 代表目前執行的模組
+
+app.secret_key = "any string but secret"
 
 
 @app.route("/")
@@ -15,11 +14,28 @@ def index():
 
 @app.route("/member")
 def member():
-    account = request.args.get("account")
-
-    # if account == "test"
-
     return render_template("member.html")
+
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
+
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    account = request.form["account"]  # 要把使用者在前端的資料抓進來後端，這是POST寫法，然後放進變數
+    password = request.form["password"]
+    if account == "test" and password == "test":  # 如果說
+        return redirect("/member")
+    else:
+        # 錯的話就導去error
+        return redirect("/error?message=自訂的錯誤訊息")
+
+
+@app.route("/signout")
+def signout():
+    return redirect("/")
 
 
 app.run(port=3000)
