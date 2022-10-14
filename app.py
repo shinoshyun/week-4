8c93d79a9130ca4edf6728e6716683e0599d0684
shinoshyun <8367shyun@gmail.com>
2022-10-14 12:11:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def member():
 
 @app.route("/error")
 def error():
-
     message = request.args.get("message", "")
     # 這是GET寫法，("message"代表網址後面的接的 EX:/error?message= , 後面的文字為預設文字(也可帶入數字))
     return render_template("error.html", message=message)
